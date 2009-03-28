c4458010eec84d11dacfdf5cc4c761b3ce4e34b7
John Flatness <john@zerocrates.org>
2009-03-28 00:30:14 +0000

    Adds token model.

---
+++ models/OaiPmhRepositoryToken.php
@@ -0,0 +1,12 @@
+<?php
+class OaiPmhRepositoryToken extends Omeka_Record
+{
+    public $id;
+    public $verb;
+    public $metadata_prefix;
+    public $cursor;
+    public $from;
+    public $until;
+    public $set;
+    public $expiration;
+}
